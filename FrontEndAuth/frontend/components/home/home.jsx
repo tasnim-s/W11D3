eb065ad55e910bc27443011c2c157528b0291da1
tsaiduzzaman <tasnim.s@nyu.edu>
2021-01-13 13:41:20 -0500

    initial commit

---
+++ FrontEndAuth/frontend/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="hero-img-frame">
+      <img className="hero-img" src="https://storage.googleapis.com/afs-prod/media/8a696a69a427415c8e34e4a16555c1cd/1000.jpeg" />
+    </div> 
+    <h1>Where birds can chirp.</h1>
+  </div>
+)
